d31f805f116564c16f4b7850322ff082cc3626df
master <dhavalepooja33@gmail.com>
2023-04-20 21:55:26 +0530

    Test  Check

---
+++ src/main/java/java/poojaDh/Abc.java
@@ -0,0 +1,16 @@
+package java.poojaDh;
+
+public class Abc {
+
+
+    public static void addiction(int a,int  b){
+
+        int sum;
+        sum=a+b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        addiction(22,78);
+    }
+}
